4e85a9f29bf66f5e4fc597f66ed1aa2faeb0eb9a
Sheikh <sheihdog325@gmail.com>
2023-01-30 17:57:39 +0600

    10 Pipes

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core'
 
-// Задача на проработку директив
-// Проработка ngFor, ngIf, ngClass
-// 1. Отрисовать массив фруктов (вывести name и price)
-// 2. Нечетные эл. массива отобразить зеленым цветом
-// 3. Четные эл. массива отобразить красным цветом
-// 4. Отобразить только продукты стоимостью больше 7
-// 5. Продукты стоимостью больше 15 добавить любой стиль,
-// который их выделит в списке (например жирный шрифт или размер шрифты сделать больше)
-
-interface Fruit {
-  id: string
-  name: string
-  price: number
-}
+// Pipes
+// https://angular.io/api/?type=pipe
+// https://angular.io/api/core/Pipe
 
 @Component({
   selector: 'inst-parent',
@@ -21,15 +10,7 @@ interface Fruit {
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
-  fruits: Fruit[] = [
-    { id: '1', name: 'apple', price: 10 },
-    { id: '2', name: 'orange', price: 20 },
-    { id: '3', name: 'watermelon', price: 30 },
-    { id: '4', name: 'banana', price: 5 },
-    { id: '5', name: 'pears', price: 12 },
-    { id: '6', name: 'raspberries', price: 18 },
-    { id: '7', name: 'avocados', price: 14 },
-    { id: '8', name: 'mangoes', price: 3 },
-    { id: '9', name: 'kiwifruit', price: 7 },
-  ]
+  title = 'Lorem ipsum dolor sit amet'
+  url = 'https://angular.io/api/commons/SlicePipe'
+  date = new Date(2022, 4, 12, 10)
 }
