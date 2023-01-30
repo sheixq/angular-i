b3af49b8d958dd15fca68db7c948b07bc10cdef2
Sheikh <sheihdog325@gmail.com>
2023-01-30 19:37:02 +0600

    11.2 Создание и регистрация сервиса, DI, модификаторы доступа (11.1 теория — Введение в сервисы и DI dependency injection)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
-import { ParentComponent } from './parent/parent.component'
-import { ChildComponent } from './parent/child/child.component'
-import { FormsModule } from '@angular/forms'
+import { CompAComponent } from './components/comp-a/comp-a.component'
+import { CompBComponent } from './components/comp-b/comp-b.component'
 
 @NgModule({
-  declarations: [AppComponent, ParentComponent, ChildComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, CompAComponent, CompBComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/comp-b/comp-b.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'inst-comp-b',
+  templateUrl: './comp-b.component.html',
+  styleUrls: ['./comp-b.component.scss'],
+})
+export class CompBComponent {}
