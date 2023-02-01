1d0d976020e563554a9556dea0bef8baabd439c7
Sheikh <sheihdog325@gmail.com>
2023-02-01 06:40:06 +0600

    12.5 Memory leak, unsubscribe (очень плохая реализация отписки — так не писать)

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Todo, TodosService } from 'src/app/services/todos.service'
 import { HttpErrorResponse } from '@angular/common/http'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'inst-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, OnDestroy {
   todos: Todo[] = []
   error = ''
 
+  subscription: Subscription | null = null
+  subscription2: Subscription | null = null
+  subscription3: Subscription | null = null
+
   constructor(private todosService: TodosService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getTodos()
   }
 
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+      this.subscription = null
+    }
+    if (this.subscription2) {
+      this.subscription2.unsubscribe()
+      this.subscription2 = null
+    }
+    if (this.subscription3) {
+      this.subscription3.unsubscribe()
+      this.subscription3 = null
+    }
+  }
+
   getTodos() {
-    this.todosService.getTodos().subscribe({
+    this.subscription = this.todosService.getTodos().subscribe({
       next: (res: Todo[]) => {
         this.todos = res
       },
@@ -31,14 +51,14 @@ export class TodosComponent implements OnInit {
   createTodo() {
     const randomNumber = Math.floor(Math.random() * 100)
     const title = 'Angular ' + randomNumber
-    this.todosService.createTodo(title).subscribe(res => {
+    this.subscription2 = this.todosService.createTodo(title).subscribe(res => {
       this.todos.unshift(res.data.item)
     })
   }
 
   delete() {
     const todoId = '3bafcb8f-b0e9-4cb0-9861-c51541a9b56a'
-    this.todosService.delete(todoId).subscribe(() => {
+    this.subscription3 = this.todosService.delete(todoId).subscribe(() => {
       this.todos = this.todos.filter(tl => tl.id !== todoId)
     })
   }
