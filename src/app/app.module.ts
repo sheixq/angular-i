99ad22fc241f6979d307a2c4a2b1d813c7e020f7
Sheikh <sheihdog325@gmail.com>
2023-02-04 23:01:06 +0600

    14.02 Базовый роутинг

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { ReactiveFormsModule } from '@angular/forms'
+import { HomeComponent } from './components/home/home.component'
+import { LoginComponent } from './components/login/login.component'
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
+import { ProfileComponent } from './components/profile/profile.component'
+import { TodosComponent } from './components/todos/todos.component'
+import { UsersComponent } from './components/users/users.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    ProfileComponent,
+    TodosComponent,
+    UsersComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'todos', component: TodosComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'profile', component: ProfileComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inst-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent {
+
+}
